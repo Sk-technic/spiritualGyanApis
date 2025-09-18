77c6ee5622eced75e4885ab789f1819e624dfc98
dev-shubhamsk <code.dev.sk@gmail.com>
2025-09-18 11:49:18 +0530

    changes

--- src/service/mailer.service.js
+++ src/service/mailer.service.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  port: 587,
   secure: true, // true if port=465
   auth: {
     user: process.env.SMTP_USER,
